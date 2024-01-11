a3f4bc8a665fb7f1940fe6b8d3ccc0ef4db7f51b
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2024-01-10 19:15:13 -0500

    added button

---
+++ components/Homepage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Typography, Button } from "@mui/material";
+
+const Homepage = () => {
+    return (
+        <>
+            <Typography varaint="h1" fontSize="3rem" display="flex" margin="0 auto" justifyContent="center">
+                Cynthia Zhang
+            </Typography>
+            <Typography varaint="h1" fontSize="3rem" display="flex" margin="0 auto" justifyContent="center">
+                Cynthia Zhang
+            </Typography>
+            <Button variant="contained">Click me</Button>
+            <img src="./img1.jpeg" width="300px" />
+            <Button>Hello</Button>
+
+
+        </>
+    )
+}
+
+export default Homepage
---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <Typography>
+            Navbrdwada
+        </Typography>
+
+    )
+}
+
+export default Navbar
